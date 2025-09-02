3aa9d9b288fa546d901458f1ca48a0008880a478
Samantha Nthambi Mbila <128149228+ykitsama@users.noreply.github.com>
2025-09-02 21:43:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -50,16 +50,3 @@ function typeWriter() {
   }
 }
 typeWriter();
-
-// === Reveal Tags on Scroll ===
-const projectCards = document.querySelectorAll(".project-card");
-
-const revealOnScroll = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
-  });
-}, { threshold: 0.2 });
-
-projectCards.forEach((card) => revealOnScroll.observe(card));
